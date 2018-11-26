8ba02e0bfaccc8950bea342febbcc29dd5043ce1
Maas Lalani <maas.lalani@shopify.com>
2018-11-26 12:44:02 -0500

    Add people and modify script to add new pages

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 while read p; do
   l=$(echo $p | tr "[:upper:]" "[:lower:]" | tr -d " " | tr -d "\n")
+  mkdir "founders/$l/"
+  cp generic.html "founders/$l/index.html" 
   sed -i '' "s/\$FOUNDER_NAME/$p/g" "founders/$l/index.html";
   sed -i '' "s/\$FOUNDER_ESCAPED/$l/g" "founders/$l/index.html";
 done <people.txt
